72c441129f3edeff11c81a8655d11a7ecbcb7fca
Boxie <forkome@gmail.com>
2022-05-16 21:25:28 +0800

    Init

--- business/sensor.py
+++ business/sensor.py
@@ -22,6 +22,23 @@ class Sensor(object):
         ajax_data = json.dumps(results)
         return ajax_data
 
+    def getSen2_ChartData(self):
+        value_results = []
+        label_results = []
+        db = database()
+        sql = """SELECT data_time,sen1 FROM (
+                    select data_time,sen2 from demo ORDER BY data_date DESC,data_time DESC LIMIT 10
+                  ) A ORDER BY data_time"""
+        print(sql)
+        cursor = db.execute_select_sql(sql)
+        for row in cursor.fetchall():
+            label_results.append(row[0])
+            value_results.append(row[1])
+        #results = Cursor2Dict(cursor)
+        results = {"LABELS": label_results, "VALUES": value_results}
+        ajax_data = json.dumps(results)
+        return ajax_data
+
 
 
 # sen = Sensor()
--- routes/sen_route.py
+++ routes/sen_route.py
@@ -9,9 +9,13 @@ sensor_bp = Blueprint('sensor_bp',__name__)
 @sensor_bp.route('/sample', methods= ['POST'])
 @cross_origin()
 def sample():
+    ajax_data = ""
     if request.method == "POST":
         data = request.form
         data_type = data.get('type')
         sensor = Sensor()
-        ajax_data = sensor.getSen1_ChartData()
+        if data_type == "sen1":
+            ajax_data = sensor.getSen1_ChartData()
+        elif data_type == "sen2":
+            ajax_data = sensor.getSen2_ChartData()
     return ajax_data
