a908dd249a0d753af351be655e675bdb8944bc87
Boxie <forkome@gmail.com>
2022-05-23 20:39:51 +0800

    Init

--- routes/sen_route.py
+++ routes/sen_route.py
@@ -1,6 +1,10 @@
+import json
+import time
+
 from flask import Blueprint, request
 from flask_cors import cross_origin
 from business.sensor import Sensor
+from jobs.datapost import SEN_DATA
 
 sensor_bp = Blueprint('sensor_bp',__name__)
 
@@ -20,3 +24,27 @@ def sample():
             ajax_data = sensor.getSen_ChartData(sen_type, from_date, to_date)
 
     return ajax_data
+
+@sensor_bp.route('/save_data', methods= ['GET'])
+@cross_origin()
+def save_data():
+    if request.method == "GET":
+        data = request.args
+        sen_data = data.get('data')
+        try:
+            if len(sen_data) > 0:
+                print(sen_data)
+                sen_data = """{"time":"","sen1":750.73,"sen2":11.13,"sen3":3.00,"sen4":4.00,"sen5":5.00,"sen6":10.67,"set1":0,"set2":0,"set3":0,"set4":0,"set5":0,"set6":0,"set7":0}"""
+                data = json.loads(sen_data)
+                print(data)
+                localtime = time.localtime()
+                data_date = time.strftime("%Y%m%d", localtime)
+                sen_data = SEN_DATA(data_date, data["time"],
+                                    data["sen1"], data["sen2"], data["sen3"], data["sen4"], data["sen5"], data["sen6"],
+                                    data["set1"], data["set2"], data["set3"], data["set4"], data["set5"], data["set6"],
+                                    data["set7"])
+                sen_data.insert_db()
+        except Exception as e:
+            print("ERROR:")
+
+    return "OK"
